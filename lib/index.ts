f0cc5dc7ca6fb5b16c14047096dfad72946bc755
iwanttobealight <iwanttobealight@yandex.ru>
2018-11-30 17:52:48 +0300

    add plus

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,7 @@ import * as React from 'react'
 
 import formatNumber from './utils/addSpacesToNumber'
 
-const PHONE_COUNTRY_CODES = ['7', '8']
+const PHONE_COUNTRY_CODES = 7
 
 interface Props {
   className?: string
@@ -19,24 +19,24 @@ interface State {
 class PhoneInput extends React.Component<Props, State> {
 
   public state = {
-    value: `${PHONE_COUNTRY_CODES[0]}`,
+    value: `${PHONE_COUNTRY_CODES}`,
   } as State
 
   public onPhoneInputChange = (evt: any) => {
 
     const { value } = evt.target
+    let v = value.substr(1, value.length)
     const prevValue = this.state.value
 
     let d = null
     try {
-      d = diff(value, prevValue)[1][1]
+      d = diff(v, prevValue)[1][1]
     } catch {
       // pass
     }
 
-    let v = value
     if (['(', ')'].includes(d)) {
-      v = value.substr(value, value.length - 1)
+      v = v.substr(v, v.length - 1)
     } else {
       if (isNaN(d)) {
         return
@@ -63,12 +63,12 @@ class PhoneInput extends React.Component<Props, State> {
         onChange: (this.onPhoneInputChange),
         // onBlur: (onBlur),
         // onFocus: (onFocus),
-        value: (this.state.value),
+        value: `+${(this.state.value)}`,
         // name: 'phone',
         type: 'text',
         // required: true,
         // autoFocus: true,
-        maxLength: 17,
+        maxLength: 18,
       },
     )
   }
